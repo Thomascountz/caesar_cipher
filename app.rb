5287abf060cbac8c4cf083126e11dc0c03a318c7
Thomas Countz <Thomascountz@gmail.com>
2016-12-21 23:04:23 +0000

    rubocop

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require 'sinatra/reloader' if development?
 require './lib/caesar_cipher.rb'
 
 get '/' do
-  params[:input_string] ? string = params[:input_string] : string = "Encrypted Message"
-  params[:shift_value]  ? index = params[:shift_value].to_i : index = 0
+  params[:input_string] ? string = params[:input_string] : string = 'Encrypted Message'
+  index = params[:shift_value] ? params[:shift_value].to_i : 0
   message = c_cypher(string: string, index: index)
   erb :index, locals: { message: message }
 end
