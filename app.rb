7d10f4dfdd09472a136787a5878dd59cb8557359
Thomas Countz <Thomascountz@gmail.com>
2016-12-31 02:07:38 +0000

    fixed styling

--- app.rb
+++ app.rb
@@ -6,5 +6,5 @@ get '/' do
   params[:input_string] ? string = params[:input_string] : string = 'Encrypted Message'
   index = params[:shift_value] ? params[:shift_value].to_i : 0
   message = c_cypher(string: string, index: index)
-  erb :index, locals: { message: message }
+  erb :index, locals: { message: message, string: string}
 end
